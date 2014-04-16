72df28c5031d18c6c36eeee721e88bec054208f8
ivan.janson <ivan.janson@gmail.com>
2014-04-16 13:39:29 +0400

    Add nav links to gh-pages sample

---
+++ public/js/theme_switcher.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('#switch-buttons a').each(function (i, btt) {
+        $(btt).click(function () {
+            if ($(this).hasClass('active'))
+                return false;
+            $('#switch-buttons a').each(function (i, btt) { $(btt).removeClass('active'); });
+            var cssPath = 'public/colors/' + this.id.replace('theme-', '') + '.css';
+            var cssAlreadyLoaded = false;
+            $('pre.source-code').each(function (j, code) {
+                $(code).fadeOut('fast', function() {
+                    if (!cssAlreadyLoaded) {
+                        cssAlreadyLoaded = true;
+                        $('link#theme').attr({href: cssPath});
+                    }
+                });
+            });
+            $('pre.source-code').each(function (j, code) { $(code).fadeIn('fast'); });
+            $(this).addClass('active');
+            return false;
+        });
+    });
+});
